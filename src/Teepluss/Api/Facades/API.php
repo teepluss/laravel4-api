3557ccc1876d0e451b113f83379ab01348badbd9
Teepluss <admin@jquerytips.com>
2013-11-18 12:24:15 +0700

    comment facade

--- src/Teepluss/Api/Facades/API.php
+++ src/Teepluss/Api/Facades/API.php
@@ -7,6 +7,9 @@ class API extends Facade {
     /**
      * Get the registered name of the component.
      *
+     * If you got a problem about this facade
+     * you may checking on sensitive case.
+     *
      * @return string
      */
     protected static function getFacadeAccessor() { return 'api'; }
